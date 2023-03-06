405f1c6ef48c386fe9418bdd9c4d1659e4c64130
Carlos <carlos.ramon.santonja@gmail.com>
2023-03-06 10:56:25 +0100

    Puta vida

---
+++ .config/.config/Code/User/History/59a10ff7/6slm.py
@@ -0,0 +1,24 @@
+import abci
+mport sys
+
+import os
+
+import urllib3
+
+a = {
+    "a": "b"
+}
+
+
+def form(xorg):
+    """Form.
+
+    Args:
+        xorg (int): things
+    """
+    return xorg
+
+
+class foo:
+    def __init__(self, hola):
+        self.hola = hola
---
+++ .config/.config/Code/User/History/59a10ff7/NV5y.py
@@ -0,0 +1,21 @@
+import abc
+import os
+import sys
+
+import urllib3
+
+a = {
+    "a": "b"
+}
+
+def form(xorg):
+    """Form.
+
+    Args:
+        xorg (int): things
+    """
+    return xorg
+
+class foo:
+    def __init__(self, hola):
+        self.hola = hola
