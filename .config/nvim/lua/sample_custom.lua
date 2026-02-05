1c6a921d4a375175670723f0acea2a5e4fe40400
fisicocarlos <carlos.ramon.santonja@gmail.com>
2026-02-05 16:21:35 +0100

    Rust analyzer a nvim

--- .config/nvim/lua/sample_custom.lua
+++ .config/nvim/lua/sample_custom.lua
@@ -2,19 +2,27 @@ local M = {}
 
 -- add extra plugins here
 M.plugins = {
-    { "Shatur/neovim-ayu" },
+  { "Shatur/neovim-ayu" },
 }
 
 -- add extra configuration options here, like extra autocmds etc.
 -- feel free to create your own separate files and require them in here
 M.configs = function()
-    require("ayu").colorscheme()
+  require("ayu").colorscheme()
 end
 
 -- add servers to be used for auto formatting here
 M.formatting_servers = {
-    rust_analyzer = {},
-    lua_ls = {},
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
+          disabled = { "unused-variable", "dead_code", "unused_fields" },
+        },
+      },
+    },
+  },
+  lua_ls = {},
 }
 
 -- add Tree-sitter to auto-install
@@ -22,17 +30,17 @@ M.ensure_installed = { "toml" }
 
 -- add any null-ls sources you want here
 M.setup_sources = function(b)
-    return {
-        b.formatting.autopep8,
-        b.formatting.prettier.with({
-            extra_filetypes = { "toml" },
-            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-        }),
-        b.formatting.black.with({
-            extra_args = { "--fast" },
-        }),
-        b.formatting.stylua,
-    }
+  return {
+    b.formatting.autopep8,
+    b.formatting.prettier.with({
+      extra_filetypes = { "toml" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    }),
+    b.formatting.black.with({
+      extra_args = { "--fast" },
+    }),
+    b.formatting.stylua,
+  }
 end
 
 return M
